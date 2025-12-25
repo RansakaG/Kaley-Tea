048b6ce6421ab3f9cbc4485b7d098ed93c9c44a6
RansakaG <ransaka@gmail.com>
2025-12-25 16:29:45 +0530

    Fix routing: switch to HashRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import VisitHeal from './pages/VisitHeal';
 import BuySipHeal from './pages/BuySipHeal';
